9d950c72bd536284760f605604fe3013e8955af5
Heitor Leal <heitorlsms@gmail.com>
2025-09-20 08:44:49 -0300

    Aula 14

--- View/login.php
+++ View/login.php
@@ -33,6 +33,27 @@
                 <i class="w3-xxlarge fa fa-enveloppe-o"></i>
             </div>
             <div class="w3-rest">
-                <input class
+                <input class="w3-input w3-border w3-round-large" name="txtEmail"
+                type="text" placeholder="Email">
+            </div>
+        </div>
+
+        <div class="w3-row w3-section">
+            <div class="w3-col" style="width:11%">
+                <i class="w3-xxlarge fa fa-lock"></i>
+            </div>
+            <div class="w3-rest">
+                <input class="w3-input w3-border w3-round-large" name="txtSenha"
+                type="password" placeholder="Senha">
+            </div>
+        </div>
+
+        <div class="w3-row w3-section">
+            <div class="w3-center" style="">
+                <button name="btnCadastrar" class="w3-button w3-block w3-margin
+                w3-blue w3-cell w3-round-large" style="width: 90%;">Cadastrar</button>
+            </div>
+        </div>
+    </form>
 </body>
 </html>
