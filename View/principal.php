1f70163c209c6a6c372a7a968e7bd83df952b706
Heitor Leal <heitorlsms@gmail.com>
2025-09-22 18:43:11 -0300

    Aula 14

---
+++ View/principal.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="hhtps://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Enlatados Juarez</title>
+    <style>
+        /* Definimos que a sidebar terá uma largura de 120px e cor a cord de fundo #222 */
+
+        .w3-sidebarr{
+            width: 120px;
+
+        }
+
+        /*Define Fonte para todas as tags listadas abaixo*/
+        body,
+        h1,h2,
+        h3,
+        h4,
+        h5,
+        h6 {
+            font-family: "Montserrat", sans-serif
+        }
+    </style>        
+</head>
+<body class="w3-light-grey">
+    <nav class="w3-sidebar w3-bar-block w3-center w3-blue">
+        <a href="#home" class="w3-bar-item w3-button w3-block n w3-cell w3-hover-light-grey w3-hover-text-cyan w3-text-light-grey">
+            <i class="fa fa-home w3-xxlarge"></i>
+            <p>HOME</p>
+        </a>
+        <a href="#dPessoais" class="w3-bar-item w3-button
+        w3-block n w3-cell w3-hover-light-grey h3-hover-text-cyan w3-text-light-grey">
+            <i class="fa fa-address-book-o w3-xxlarge"></i>
+            <p>Dados Pessoais</p>
+        </a>
+        <a href="#formacao" class="w3-bar-item w3-button w3-block n w3-cell 
+        w3-hover-light-grey w3-hover-text-cyan w3-text-light-grey">
+            <i class="fa fa-mortar-board w3-xxlarge"></i>
+            <p>Formação</p>
+        </a>
+    </nav>
+    <div class="w3-padding-large" id="main">
+    </div>
+</body>
+</html>
